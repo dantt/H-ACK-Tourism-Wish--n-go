05430d6ea89c34e4952be9aadfe35cc34a2a16a2
Daniele <daniele_lain@libero.it>
2014-04-06 07:42:51 +0200

    This is WORKING FOR REAL

--- views/CustomEvents.js
+++ views/CustomEvents.js
@@ -3,6 +3,46 @@
     var startPoint;
     var prevY = 0;
     var image_index = 1;
+    
+    
+    
+    
+      
+      var hotels;
+      var globalHotelId;
+      loadPanelVisible = ko.observable(false);
+	loadPanelVisible(true);
+	Middle.getProvidersByDistance(45.564, 12.428, function(partners) {
+	  hotels = partners;
+	  loadPanelVisible(false);
+	  father = $("#cont");
+	  var hotel = hotels.shift();
+	  console.log(hotel);
+	  globalHotelId = hotel.Id;
+	  new_div =$("<div id='hotel" + hotel.Id + "' style='background-image: url(\"http://h-ack-wd.azurewebsites.net" + hotel.Photos[0].Url + "\"); width:100%; background-size: auto 100%; background-repeat: no-repeat; background-position: center top; height: 400px;max-height:400px;'></div>");
+	  //new_div.hide();
+	  father.html(new_div);
+	  $('#Nome').html(hotel.Name);
+	  $('#Indirizzo').html(hotel.Address + ' - ' + hotel.Location);
+	  
+	});
+	
+	
+	function appendimage() {
+	  father = $("#cont");
+	  var hotel = hotels.shift();
+	  console.log(hotel);
+	  globalHotelId = hotel.Id;
+	  new_div =$("<div id='hotel" + hotel.Id + "' style='background-image: url(\"http://h-ack-wd.azurewebsites.net" + hotel.Photos[0].Url + "\"); width:100%; background-size: auto 100%; background-repeat: no-repeat; background-position: center top; height: 400px;max-height:400px;'></div>");
+	  //new_div.hide();
+	  father.html(new_div);
+	  $('#Nome').html(hotel.Name);
+	  $('#Indirizzo').html(hotel.Address + ' - ' + hotel.Location);
+	};
+	
+      
+      
+      
    
     var pointerEvents = [
             { name: "dxpointerdown", displayName: "down" },
@@ -54,18 +94,14 @@
       element.on("dxpointerup",function(e){
 	if (isDragging == true){
 	  isDragging = false;
-	  father = $("#cont");
-	  image_index++;
-	  image_name = "hotel" + image_index + ".jpg";
-	  new_div =$("<div id='hotel" + image_index + "' style='position: absolute; background-image: url(\"" + image_name + "\"); width:100%; background-size: auto 100%; background-repeat: no-repeat; background-position: left top; height: 100%;max-height:400px;'></div>");
-	  new_div.hide();
-	  father.append(new_div);
-	  new_div.fadeIn("slow");
-	  create_events($('#hotel'+image_index));
-	  
+	  createImage();
 	}
       });
     };
+    
+    
+    
+    
     var sourceSwap = function () {
       var $this = $(this);
       var newSource = $this.data('alt-src');
@@ -82,15 +118,18 @@
       $('#img_cross').hover(sourceSwap,sourceSwap);
       $('#img_like').hover(sourceSwap,sourceSwap);
       $('#img_like').on("dxpointerdown", function(e){
-	$('#hotel'+image_index).animate({left: "+=5000"}, 2000);
-	isDragging = true;
-	$('#hotel'+image_index).trigger("dxpointerup");
+	appendimage();
       });
     };
     
     viewModel.viewShown = function() {
-      create_events($('#hotel'+image_index));
-      image_events();
+      $('#img_like').on("dxpointerdown", function(e){
+	Middle.addPreference(globalHotelId);
+	appendimage();
+      });
+      $('#img_cross').on("dxpointerdown", function(e){
+	appendimage();
+      });
     };
     
     return viewModel;
