9a36a0c74b9fad909da7e6e24cdac4ceaac8543f
Tommaso Frassetto <tomfox91@libero.it>
2014-04-06 08:37:42 +0200

    Punti & Non più punti

--- views/Buy.js
+++ views/Buy.js
@@ -4,6 +4,12 @@
     var s = (Middle.getUser().saldo || 0.0);
     viewModel.saldo = ko.observable('' + s.toFixed(2) + ' p');
     
+    viewModel.viewShown = function() {
+        var s = (Middle.getUser().saldo || 0.0);
+        viewModel.saldo('' + s.toFixed(2) + ' p');
+    }
+
+    
     viewModel.addFondi = function(n) {
         var s = (Middle.getUser().saldo || 0.0);
         var u = {
